ee52c07404fb63269ce2b8207bdb5db96fd9e1aa
Roee <roeebibi@gmail.com>
2024-12-26 18:49:16 +0200

    delete old project: chessProject

--- chessProject/Rook.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Piece.h"
-class Rook : public Piece
-{
-public:
-	Rook(int x, int y, bool color) : Piece(x, y, 'r', color) {}
-	bool virtual isValidMovement(const Piece& dest, const std::vector<std::vector<Piece*>>& board) const;
-};
